3095e3dcda6d23430eccb0a9dd5326791956f383
Tajbir23 <studenttajbirislam@gmail.com>
2024-11-10 11:29:08 +0600

    test

--- models/userSchema.js
+++ models/userSchema.js
@@ -35,6 +35,10 @@ const userSchema = new Schema({
     status: {
         type: String,
         default: "pending"
+    },
+    role: {
+        type: String,
+        default: "user"
     }
 },
     {
