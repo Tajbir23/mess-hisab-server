eb8de888e29ce75325c04f3266ec89ac0084fdfe
Tajbir23 <studenttajbirislam@gmail.com>
2024-11-11 14:28:45 +0600

    fix: post meals update issue

--- controllers/meal/postMeal.js
+++ controllers/meal/postMeal.js
@@ -7,15 +7,15 @@ const postMeal = async(req, res) => {
     const date = new Date().toISOString().slice(0,10)
     try {
         if(body.breakfast){
-            await mealModel.create({user: id, date: date, breakfast: body.breakfast})
+            await mealModel.updateOne({user: id, date}, {$set : {breakfast: body.breakfast}, $setOnInsert: {user: id, date}},{upsert: true, new: true})
             await userModel.findByIdAndUpdate(id, {todayBreakfast: true})
         }
         if(body.dinner){
-            await mealModel.create({user: id, date: date, dinner: body.dinner})
+            await mealModel.updateOne({user: id, date: date}, {$set: {dinner: body.dinner}, $setOnInsert: {user: id, date}},{upsert: true, new: true})
             await userModel.findByIdAndUpdate(id, {todayDinner: true})
         }
         if(body.extraCost){
-            await mealModel.create({user: id, date: date, extraCost: body.extraCost})
+            await mealModel.updateOne({user: id, date: date}, {$set: {extraCost: body.extraCost}, $setOnInsert: {user: id, date}},{upsert: true, new: true})
             await userModel.findByIdAndUpdate(id, {todayExtraCost: true})
         }
         res.status(200).send({
